60ce792318e1507f079e83e49c82adc68a4bd002
Polina <ms.maylin@inbox.ru>
2023-01-17 22:39:30 +0300

    Task 4 added

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int min = 2;
+
+if (number >= min)
+{
+    while (min < number)
+    {
+        Console.Write($"{min} ");
+        min = min + 2;
+    }
+}
+else
+{
+    Console.Write($"Четных чисел от 1 до {number} нет, поскольку {number} меньше 1.");
+}
